3e13cc41b8e8733cc2d4afbe4d13c934d6210207
Courtney Angotti <courtneyangotti@gmail.com>
2019-05-03 22:38:44 -0700

    practice html added

--- ajax.js
+++ ajax.js
@@ -5,11 +5,29 @@ $.ajax("https://www.reddit.com/r/popular/top/.json?count=20", {
     var row = data.data.children;
     console.log("SUCCESSFULLY GOT LIST");
 
+    //RICARDO:   GET THE POST-LIST FROM YOUR HTML PAGE
+    let post_list = document.querySelector(".post-list");
+
+
     for (var i = 0, l = row.length; i < l; i++) {
       var post = row[i].data;
       var votes = post.score;
       var title = post.title;
       var subtime = post.created_utc;
+
+      //RICARDO: NOW CREATE AN ELEMENT TO PUT IN THE LIST, ILL USE THE CARD
+      var postCard = document.createElement("div");
+      // SET THE CLASS
+      postCard.className ="card item1";
+      //YOU MAKE ANOTHER ELEMENT TO PUT INSIDE THE CARD
+      var titleElement = document.createElement("div");
+      //SET THE TEXT OD THE SECOND ELEMENT TO TITLE
+      titleElement.innerText = title;
+      //APPEND TITLE TO CARD
+      postCard.appendChild(titleElement);
+
+      //FINALLY APPEND CARDO TO LIST
+      post_list.appendChild(postCard);
       console.log(title);
     }
 
